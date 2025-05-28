36b860289cb20582d6b96c976aadda1102670a57
Aparna-coder-tech <aparnap4@kpmg.com>
2025-05-28 15:28:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ session = cnx.session()
 
 
 #my_dataframe = session.table("smoothies.public.fruit_options").select (col('fruit_name'),col('search_on'))
-my_dataframe = session.table("smoothies.public.fruit_options")
+my_dataframe = session.table("smoothies.public.fruit_options").select (col('fruit_name'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
